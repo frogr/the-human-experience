cfbac6e1d25eab8b5aa47e68450d99f123dfb768
Austin F <austindanielfrench@gmail.com>
2018-04-22 17:35:35 -0700

    initialized server.js and set up basic mongo connection

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 8080;
+const STATUS_USER_ERR = 422;
+const STATUS_SERVER_ERR = 500;
+const STATUS_OTHER_ERR = 69;
+const STATUS_GOOD = 200;
+
+
+
+// << plumbing >>
+mongoose.Promise = global.Promise;
+const connect = mongoose.connect('mongodb://localhost/the');
+
+connect.then(
+  () => {
+    app.listen(PORT);
+    console.log(`listening! port: ${PORT}`);
+  },
+  err => {
+    throw new Error(`!!E: error! info here: ${err}`)
+  }
+);
