58c2be2a09f2415411bd8f9c79622740a9fd2961
Austin F <austindanielfrench@gmail.com>
2018-04-24 18:18:48 -0700

    i forgot that i should be setting up an API

--- api/models/Poll.js
+++ api/models/Poll.js
@@ -13,18 +13,38 @@ const PollSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  optionOneVotes: {
+    type: Number,
+    default: 0;
+  }
   optionTwo: {
     type: String,
     required: true
   }
+  optionTwoVotes: {
+    type: Number,
+    default: 0;
+  }
   optionThree: {
     type: String,
+  }
+  optionThreeVotes: {
+    type: Number,
+    default: 0;
   }
   optionFour: {
     type: String,
   }
+  optionFourVotes: {
+    type: Number,
+    default: 0;
+  }
   optionFive: {
     type: String,
+  }
+  optionFiveVotes: {
+    type: Number,
+    default: 0;
   }
 });
 
